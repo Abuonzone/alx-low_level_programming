6760f22d54eb29c5ec84e6b4b730d6c005c1f29c
Abuonzone <auduabubakar1992@gmail.com>
2022-07-01 22:42:43 +0000

    C - Variables, if, else, while: Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char n = 0 ; n < 10; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
