92d167eadac2d6b219d759853e03feff022128a8
Abuonzone <auduabubakar1992@gmail.com>
2022-07-01 21:47:16 +0000

    C - Variables, if, else, while: Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n = 'a';
+	char x = 'A';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n = n + 1;
+	}
+	while (x <= 'Z')
+	{
+		putchar(x);
+		x = x + 1;
+	}
+	putchar('\n');
+	return (0);
+}
