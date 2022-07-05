e1a301f6c18026fa86ba7769fdb842ad853981fd
Abuonzone <auduabubakar1992@gmail.com>
2022-07-05 21:27:43 +0000

    Functions_Nested_Loops: Task 0 update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_putchar\n");
 	return (0);
 }
