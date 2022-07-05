03b13384aff6a575d2b8cc4e8a929bbbe3556100
Abuonzone <auduabubakar1992@gmail.com>
2022-07-05 22:04:24 +0000

    Functions_Nested_Loops: Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print lowercase alphabets 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int x;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
