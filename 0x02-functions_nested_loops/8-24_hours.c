b2d3ba127e556ccc2978aeb82ed89570bd24ac4b
Abuonzone <auduabubakar1992@gmail.com>
2022-07-06 00:00:34 +0000

    Functions_Nested_Loops: Task 8 update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,41 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 23; b++)
+		if (a == 2)
 		{
-			for (c = 0; c <= 5; c++)
+			for (b = 0; b < 4; b++)
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c < 6; c++)
 				{
-					e = b % 10;
-					_putchar(a + 48);
-					_putchar(e + 48);
-					_putchar(':');
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
+					for (d = 0; d <= 9; d++)
+					{
+						e = b % 10;
+						_putchar(a + 48);
+						_putchar(e + 48);
+						_putchar(':');
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+		else
+		{
+			for (b = 0; b <= 9; b++)
+			{
+				for (c = 0; c < 6; c++)
+				{
+					for (d = 0; d <= 9; d++)
+					{
+						e = b % 10;
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(':');
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}
 				}
 			}
 		}
