30f2fc91f502eb2a00b8a718110256ccfef1243e
Abuonzone <auduabubakar1992@gmail.com>
2022-07-07 22:27:26 +0000

    More Functions - Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - Draw a straight line in the terminal
+ * @n: parameter
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		i = 1;
+		while (i <= n)
+		{
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
