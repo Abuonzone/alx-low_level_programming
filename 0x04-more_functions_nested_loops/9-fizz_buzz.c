e87740f56a774da449880ba73e9c88b061e63520
Abuonzone <auduabubakar1992@gmail.com>
2022-07-07 23:03:51 +0000

    More Functions - Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+}
