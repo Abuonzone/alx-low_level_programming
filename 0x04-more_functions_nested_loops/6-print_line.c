15423e441dfe9df60adf8d46cf5c572f522823d7
Abuonzone <auduabubakar1992@gmail.com>
2022-07-07 22:18:43 +0000

    More Functions - Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - Draw a straight line in the terminal
+ * @n: parameter
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		i = 0;
+		while (i <= n)
+		{
+			_putchar('-');
+			i++;
+		}
+	}
+}
