b70bfd10fb2cccede451c0f084ceb9ab8568b0d3
Abuonzone <auduabubakar1992@gmail.com>
2022-07-08 22:55:18 +0000

    Debugging: Task 2 update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,14 +10,14 @@
 
 int largest_number(int a, int b, int c)
 {
-	if ((a > b && b > c) || (a > c && c > b) )
+	if ((a > b && b > c) || (a > c && c > b))
 	{
-		return a;
+		return (a);
 	}
 	else if ((b > a && a > c) || (b > c && c > a))
 	{
-		return b;
+		return (b);
 	}
 	else
-		return c;
+		return (c);
 }
