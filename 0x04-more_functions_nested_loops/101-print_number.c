2f2fa98b37b03b3df8ba69a5ae7e5de34a2c1924
Abuonzone <auduabubakar1992@gmail.com>
2022-07-10 14:08:28 +0000

    More Functions: Task 12 update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,8 +8,17 @@
 
 void print_number(int n)
 {
-	if (typeof(n) == int)
+	if (n < 0)
 	{
-		_putchar(n + '0');
+		_putchar('-');
+		n = -n;
 	}
+
+	int k = n / 10;
+	int j = n % 10;
+	
+	if (k)
+		print_number(k);
+
+	_putchar(j + '0');
 }
