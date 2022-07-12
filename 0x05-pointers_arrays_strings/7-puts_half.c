02d4167ff9eacc7bb384f94e45d0d4be7c089e37
Abuonzone <auduabubakar1992@gmail.com>
2022-07-12 22:13:09 +0000

    Pointers, Arrays & Strings: Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Print every other character of the string
+ * @str: pointer paramter
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int x = strlen(str);
+	int n = x\2;
+
+	for (i = n; i < x; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
