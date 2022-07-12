55c8f14cddb83368bee1fb8f2e5efab088738623
Abuonzone <auduabubakar1992@gmail.com>
2022-07-12 19:47:12 +0000

    Pointers, Arrays & Strings: Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Swap values of 2 integers
+ * @a: pointer parameter 1
+ * @b: pointer parametr 2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
