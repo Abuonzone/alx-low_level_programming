e8f81a0e087e45b59cd35a2edf5669b96677f592
Abuonzone <auduabubakar1992@gmail.com>
2022-07-12 20:13:08 +0000

    Pointers, Arrays & Strings: Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: point parameter
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
