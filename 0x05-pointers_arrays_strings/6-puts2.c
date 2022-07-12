c1b39abef94b6bcfdc69e28636d56bed4c92f781
Abuonzone <auduabubakar1992@gmail.com>
2022-07-12 22:05:01 +0000

    Pointers, Arrays & Strings: Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - Print every other character of the string
+ * @str: pointer paramter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	_putchar('\n');
+}
