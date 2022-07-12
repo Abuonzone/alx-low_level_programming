aabadfabac1b1aeb1f5a70a170df562c9a3c56a0
Abuonzone <auduabubakar1992@gmail.com>
2022-07-12 20:01:49 +0000

    Pointers, Arrays & Strings: Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Calculate the lenght of the string
+ * @s: pointer parameter
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return strlen(*s);
+}
