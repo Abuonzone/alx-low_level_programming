645a6109e8174a9519aa7e47c21a271c3c0ad473
Abuonzone <auduabubakar1992@gmail.com>
2022-07-14 23:21:46 +0000

    More Pointers, Arrays & Strings: Task 5 update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,14 +10,14 @@
 
 char *string_toupper(char *n)
 {
-	int i;
-	int str_len = strlen(n);
+	size_t i;
+	size_t str_len = strlen(n);
 
 	for (i = 0; i < strlen(n); i++)
 	{
 		if(!(isupper(n[i])))
 		{
-			n[i] = toupper(n[i]);
+			n[i] = toupper((unsigned char)n[i]);
 		}
 	}
 	return (n);
