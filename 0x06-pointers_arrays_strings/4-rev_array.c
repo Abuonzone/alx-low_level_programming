9b17b81017d00d8e1d2b5e9795cc4f9cbe4f5163
Abuonzone <auduabubakar1992@gmail.com>
2022-07-14 23:04:40 +0000

    More Pointers, Arrays & Strings: Task 4 update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,17 +10,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int y = sizeof(a) / sizeof(int);
-	int b[y];
-	int i, x, c;
+	int value, i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		b[i] = a[(n - 1) - i];
-	}
-	c = sizeof(b) / sizeof(int);
-	for (x = 0; x < c; x++)
-	{
-		a[x] = b[x];
+		value = a[i];
+		a[i] = a[(n - i) - 1];
+		a[(n - i) - 1] = value;
 	}
 }
