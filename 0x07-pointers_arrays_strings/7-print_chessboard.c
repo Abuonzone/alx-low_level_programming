140f33febff1b11506deac510c05c7066269efaf
Abuonzone <auduabubakar1992@gmail.com>
2022-07-19 22:41:58 +0000

    More Pointers, Arrays & Strings - Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print the chessboard
+ * @a: Two dimensional array
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	int row = sizeof(a) / sizeof(a[0]);
+	int column = sizeof(a[0]) / sizeof(a[0][0]);
+
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < column; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
