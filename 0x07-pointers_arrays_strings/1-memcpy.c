c2dd8b23e86611c448738975347f193c701f3e8f
Abuonzone <auduabubakar1992@gmail.com>
2022-07-19 21:50:30 +0000

    More Pointers, Arrays & Strings - Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - A Function that copies a memory area
+ * @dest: The destination parameter
+ * @src: The Source parameter
+ * @n: N bytes of src to be copied
+ * Return: char pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest, char, n));
+}
