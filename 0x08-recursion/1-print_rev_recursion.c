0674d0b72337e686b06ac2d1d4f5fe6fd3097d45
Abuonzone <auduabubakar1992@gmail.com>
2022-07-21 20:53:36 +0000

    Recursion - Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to string parameter
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i, j;
+	int str_len = strlen(s);
+	char b[str_len];
+
+	for (i = 0; i < str_len && s[i] != '\0'; i++)
+	{
+		*(b + i) = *(s + (str_len - (i + 1)));
+	}
+	for (j = 0; j < str_len && b[j] != '\0'; j++)
+	{
+		*(s + j) = *(b + j);
+		_putchar(s[j]);
+	}
+}
