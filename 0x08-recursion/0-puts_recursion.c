2faa19fcc82d23094f900d07bf0154cee1301bcf
Abuonzone <auduabubakar1992@gmail.com>
2022-07-21 20:40:52 +0000

    Recursion - Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Print a string followed by a new line
+ * @s: pointer to string parameter
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
