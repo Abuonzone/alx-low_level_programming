234f06a393c372bc162e097976cd3de4e702ffb6
Abuonzone <auduabubakar1992@gmail.com>
2022-07-21 22:53:15 +0000

    Recursion: Task 1 update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,16 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i, j;
-	int str_len = strlen(s);
-	char b[str_len];
-
-	for (i = 0; i < str_len && s[i] != '\0'; i++)
+	if (*s)
 	{
-		b[i] = s[str_len - (i + 1)];
-	}
-	for (j = 0; j < str_len && b[j] != '\0'; j++)
-	{
-		_putchar(b[j]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
