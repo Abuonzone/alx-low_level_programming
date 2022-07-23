932ba6cd12afeba2cb0bf0907d33261b5acf203b
Abuonzone <auduabubakar1992@gmail.com>
2022-07-23 00:46:26 +0000

    argc & argv: Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that ptints it name
+ * @argc: Argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc,__attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
