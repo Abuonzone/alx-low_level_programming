6c16a01a5468bff6bfc0f08e2085e7dfb8e4dfba
Abuonzone <auduabubakar1992@gmail.com>
2022-07-23 01:20:00 +0000

    argc & argv: Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that ptints it name
+ * @argc: Argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int ans = 0;
+	int err;
+
+
+	if (argc == 0)
+	{
+		putchar(48);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]))
+		{
+			ans += atoi(argv[i]);
+		}
+		else
+		{
+			err = 1;
+			break;
+		}
+	}
+	if (err == 1)
+	{
+		printf("Error\n");
+	}
+	else
+		printf("%d\n", ans);
+
+	return (0);
+}
