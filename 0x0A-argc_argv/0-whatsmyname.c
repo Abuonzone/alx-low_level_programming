2759de10486895c78a0b7e7fdce6e6e9b722379a
Abuonzone <auduabubakar1992@gmail.com>
2022-07-23 00:32:07 +0000

    argc & argv: Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that ptints it name
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
