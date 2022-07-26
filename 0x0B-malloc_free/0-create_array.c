3de824c65a2d6d103f51d788a6036848d7b1c77e
Abuonzone <auduabubakar1992@gmail.com>
2022-07-26 21:48:24 +0000

    Malloc & Free: Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_array - create an array of characters
+ * @size: size of the array parameter
+ * @c: parameter
+ * Return: Null if size = 0, or pointer to the array or null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	str = malloc(sizeof(char) * size);
+	str[0] = c;
+	return (str);
+}
+
+
