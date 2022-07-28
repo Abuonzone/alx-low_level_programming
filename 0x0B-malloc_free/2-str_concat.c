5d1d788e8ecf78a9ad9f2c4204cb459a8e8335c1
Abuonzone <auduabubakar1992@gmail.com>
2022-07-28 21:30:13 +0000

    Malloc and Free: Task 2 update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,10 +20,14 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++);
-	for (j = 0; s2[j] != '\0'; j++);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
 	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
 
 	for (k = 0; k < i; k++)
 	{
