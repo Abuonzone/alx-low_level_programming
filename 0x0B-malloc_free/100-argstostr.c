502dae82d57c82475f1db82a4d83169cd95ab848
Abuonzone <auduabubakar1992@gmail.com>
2022-07-29 00:13:23 +0000

    Malloc & Free: Task 5 update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,30 +14,27 @@ char *argstostr(int ac, char **av)
 	int i, j, x;
 	int n = 0;
 	char *str;
-	
+
 	if (ac == 0)
 		return (NULL);
-	
 	for (i = 0; i < ac; i++)
 	{
 		if (av[i] == NULL)
+		{
 			return (NULL);
-	
+		}
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
 			n++;
 		}
 		n++;
 	}
-
 	str = malloc((n + 1) * sizeof(char));
-	
 	if (str == NULL)
 	{
 		free(str);
 		return (NULL);
 	}
-
 	for (i = j = x = 0; x < n; j++, x++)
 	{
 		if (av[i][j] == '\0')
@@ -53,6 +50,5 @@ char *argstostr(int ac, char **av)
 		}
 	}
 	str[x] = '\0';
-
 	return (str);
 }
