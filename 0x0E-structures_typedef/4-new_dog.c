dbca1e1117b7a350303839b8b399391991c6fdff
Abuonzone <auduabubakar1992@gmail.com>
2022-08-04 22:23:32 +0000

    Structures and Typedef: Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ * Return: Struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, k;
+	char *dog;
+
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->name = malloc(i * sizeof(dog->name));
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		dog->name[k] = name[k];
+	dog->age = age;
+	dog->owner = malloc(j * sizeof(dog->owner));
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		dog->owner[k] = owner[k];
+	return (dog);
+}
