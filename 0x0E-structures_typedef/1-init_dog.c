280fe5e2e490c6b2a6addfc927e830d86963e993
Abuonzone <auduabubakar1992@gmail.com>
2022-08-04 21:19:08 +0000

    Structures and Typedef: Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a variable of type struct dog
+ * @d: Pointer to the new struct dog
+ * @name: Pointer to name of dog
+ * @age: Age of dog parameter
+ * @owner: pointer to owner of dog parameter
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
