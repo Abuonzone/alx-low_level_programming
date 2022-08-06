e6f7189c5afa9cbb5d79c565a20618dcda03f232
Abuonzone <auduabubakar1992@gmail.com>
2022-08-06 01:15:31 +0000

    Function Pointers: Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Function Pointers example
+ * @array: param 1
+ * @size: param 2
+ * @action: function pointer param
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
+}
