c7ac0e0dcea84682a2e53f908e7c11c0755e95af
Abuonzone <auduabubakar1992@gmail.com>
2022-08-13 18:05:01 +0000

    Singly linked lists: Task 5 update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,9 @@
  * myStartup - entry point
  * Return: 0
  */
-void myStartup (void) __attribute__ ((constructor));
+void myStartup(void) __attribute__ ((constructor));
 void myStartup(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
