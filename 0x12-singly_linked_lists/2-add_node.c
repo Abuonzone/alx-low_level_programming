67d72adc54e39e9737f89cbbf8b646d9a919b3af
Abuonzone <auduabubakar1992@gmail.com>
2022-08-13 22:23:35 +0000

    Singly linked lists: Task 2 update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,8 +11,14 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int i;
 
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->str = strdup(str);
-	if (!new_node || !new_node->str)
+	if (!new_node->str)
 	{
 		free(new_node->str);
 		free(new_node);
