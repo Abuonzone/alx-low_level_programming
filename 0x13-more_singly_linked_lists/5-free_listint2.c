bc7ae7229f8399ef3329ba4bb402ec50c4300b8e
Abuonzone <auduabubakar1992@gmail.com>
2022-08-15 20:46:07 +0000

    More singly linked lists: Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a linked list
+ * @head: linked list to free
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	if (*head != NULL)
+	{
+		free(*head);
+	}
+	while (current->next != NULL)
+	{
+		current = current->next;
+		free(current);
+	}
+}
