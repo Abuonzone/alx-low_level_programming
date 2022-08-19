f98790dffa1e933dd04f98a2415c216010a2dbae
Abuonzone <auduabubakar1992@gmail.com>
2022-08-19 20:53:00 +0000

    Bit Manipulation: Task 0 update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,16 +17,19 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	for (i = len - 1; i >= 0; i--)
+	else
 	{
-		if (b[i] == '0' || b[i] == '1')
+		for (i = len - 1; i >= 0; i--)
 		{
-			if (b[i] == '1')
-				ans += base;
+			if (b[i] == '0' || b[i] == '1')
+			{
+				if (b[i] == '1')
+					ans += base;
+			}
+			else
+				return (0);
+			base = base * 2;
 		}
-		else
-			return (0);
-		base = base * 2;
+		return (ans);
 	}
-	return (ans);
 }
