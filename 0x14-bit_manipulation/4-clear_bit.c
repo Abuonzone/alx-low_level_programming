1e8481778b0b2ee5d5307881313814314afc1b45
Abuonzone <auduabubakar1992@gmail.com>
2022-08-21 22:01:14 +0000

    Bit Manipulation: Task 4 update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	*n = (*n & (~(1 << index)));
-	if (n)
+	if (index < 63)
+	{
+		*n = (*n & (~(1 << index)));
 		return (1);
+	}
 	else
 		return (-1);
 }
