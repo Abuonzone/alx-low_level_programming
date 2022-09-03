24b4788f0b2c02b23bb80c5f0fc900545a64b9d3
Abuonzone <auduabubakar1992@gmail.com>
2022-09-03 23:43:24 +0000

    File I/O: Task 1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	for (i = 0; text_content[i]; i++)
 		;
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		write(fd, "", 1);
 	else
 		write(fd, text_content, i);
-	
+
 	close(fd);
 	return (1);
 }
