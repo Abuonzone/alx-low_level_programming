85089bfd849c9914b73603968b88e8ee58a5f13c
Abuonzone <auduabubakar1992@gmail.com>
2022-09-03 23:39:32 +0000

    File I/O: Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - Create a function that creates a file
+ * @filename: file to create
+ * @text_content: text to add
+ * Return: 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	for (i = 0; text_content[i]; i++)
+		;
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		write(fd, "", 1);
+	else
+		write(fd, text_content, i);
+	
+	close(fd);
+	return (1);
+}
