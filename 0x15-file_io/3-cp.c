73536ce79d8a77fa408f44cba27255748cbd031a
Abuonzone <auduabubakar1992@gmail.com>
2022-09-09 23:42:57 +0000

    FILE I/O: Task 3 update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,27 +19,43 @@ int main(int ac, char **av)
 	if (ac > 3)
 	{
 		exit(97);
-		dprintf(STDERR_FILENO,"cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "cp file_from file_to\n");
 	}
 
 	fd1 = open(av[1], O_RDONLY);
-	cp1 = read(fd1, buff, sizeof buff);
-	if (fd1 == -1 || cp1 < 0)
+	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't read from file %s\n", av[1]);
 		exit(98);
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", av[1]);
+	}
+	cp1 = read(fd1, buff, sizeof(buff));
+	if (cp1 < 0)
+	{
+		exit(98);
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", av[1]);
+	}
+	if (close(fd1) < 0)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd1);
 	}
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	cp2 = write(fd2, buff, cp1);
-	if (fd2 == -1 || cp2 < 0)
+	if (fd2 == -1)
 	{
 		exit(99);
-		dprintf(STDERR_FILENO,"Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]);
 	}
-
-
-	close(fd1);
-	close(fd2);
+	cp2 = write(fd2, buff, cp1);
+	if (cp2 < 0)
+	{
+		exit(99);
+		dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]);
+	}
+	if (close(fd2) < 0)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2);
+	}
 
 	return (0);
 }
